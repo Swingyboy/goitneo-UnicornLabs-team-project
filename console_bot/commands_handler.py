61ca3f4cc9d0d73ccaf19b7702c0ddf62d606bb4
serhii <sbezuhlyi@tektelic.com>
2024-03-05 13:45:23 +0200

    add AttributeError handling

--- console_bot/commands_handler.py
+++ console_bot/commands_handler.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from contacts import Record
 
 
@@ -5,6 +7,11 @@ def input_error_handler(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
+        except AttributeError as a_ex:
+            if func.__name__ == "_show_birthday":
+                return "Birthday for this contact was not specified."
+            else:
+                return str(a_ex)
         except ValueError as v_ex:
             if func.__name__ == "_add_contact":
                 return "Invalid number of arguments for add command, please try again. Give me name and phone please."
@@ -13,6 +20,9 @@ def input_error_handler(func):
                         " Give me name, old phone and new phone numbers please.")
             elif func.__name__ == "_get_phone":
                 return "Invalid number of arguments for phone command, please try again. Give me name please."
+            elif func.__name__ == "_add_birthday":
+                return ("Invalid number of arguments for add-birthday command, "
+                        "please try again. Give me name and birthday please.")
             else:
                 return str(v_ex)
         except IndexError as i_ex:
@@ -58,22 +68,30 @@ class DefaultCommandHandler:
     @input_error_handler
     def _add_phone(self, *args) -> str:
         name, phone = args
-        record = self.book.find(name)
-        if record:
-            record.add_phone(phone)
+        result = self._check_contact_exist(name)
+        if isinstance(result, Record):
+            result.add_phone(phone)
             return f"Phone number {phone} has been added to contact {name.capitalize()}."
-        else:
-            return f"Contact {name.capitalize()} does not exist."
+        return result
 
     @input_error_handler
     def _change_contact(self, *args) -> str:
         name, old_phone, new_phone = args
+        result = self._check_contact_exist(name)
+        if isinstance(result, Record):
+            result = result.edit_phone(old_phone, new_phone)
+            if result:
+                return f"Phone number {old_phone} has been changed to {new_phone} for contact {name.capitalize()}."
+            else:
+                return f"Phone number {old_phone} does not exist for contact {name.capitalize()}."
+        return result
+
+    def _check_contact_exist(self, name: str) -> Union[Record, str]:
         record = self.book.find(name)
-        if record:
-            record.edit_phone(old_phone, new_phone)
-            return f"Contact {name.capitalize()} has been updated."
-        else:
+        if not record:
             return f"Contact {name.capitalize()} does not exist."
+        else:
+            return record
 
     def _get_all(self) -> None:
         for record in self.book.get_all_records():
@@ -82,30 +100,27 @@ class DefaultCommandHandler:
     @input_error_handler
     def _get_phone(self, *args) -> str:
         name = args[0]
-        record = self.book.find(name)
-        if not record:
-            return f"Contact {name.capitalize()} does not exist."
-        else:
-            return f"{name.capitalize()}: {', '.join(phone.value for phone in record.phones)}"
+        result = self._check_contact_exist(name)
+        if isinstance(result, Record):
+            return f"{name.capitalize()}: {', '.join(phone.value for phone in result.phones)}"
+        return result
 
     @input_error_handler
     def _add_birthday(self, *args) -> str:
         name, birthday = args
-        record = self.book.find(name)
-        if not record:
-            return f"Contact {name.capitalize()} does not exist."
-        else:
-            record.add_birthday(birthday)
+        result = self._check_contact_exist(name)
+        if isinstance(result, Record):
+            result.add_birthday(birthday)
             return f"Birthday for {name.capitalize()} has been added."
+        return result
 
     @input_error_handler
     def _show_birthday(self, *args) -> str:
         name = args[0]
-        record = self.book.find(name)
-        if not record:
-            return f"Contact {name.capitalize()} does not exist."
-        else:
-            return f"{name.capitalize()}: {record.birthday.value}"
+        result = self.book.find(name)
+        if isinstance(result, Record):
+            return f"{name.capitalize()}: {result.birthday.value}"
+        return result
 
     @input_error_handler
     def _get_birthdays_per_week(self) -> None:
