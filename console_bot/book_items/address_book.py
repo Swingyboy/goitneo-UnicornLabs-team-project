e3e7541d50c30f7a6353db477b4e46f49449250f
serhii <sbezuhlyi@tektelic.com>
2024-03-13 00:36:47 +0200

    remove ellipsis

--- console_bot/book_items/address_book.py
+++ console_bot/book_items/address_book.py
@@ -1,7 +1,7 @@
 from calendar import day_name
 from collections import UserDict, defaultdict
 from datetime import datetime
-from typing import Optional, List, Dict, Union
+from typing import Any, Optional, List, Dict, Union
 
 from fields.record import Record
 
@@ -28,7 +28,7 @@ class AddressBook(UserDict):
         """Return all records in the address book."""
         return list(self.data.values())
 
-    def to_dict(self) -> List[Dict[str, ...]]:
+    def to_dict(self) -> List[Dict[str, str]]:
         """Convert the address book to a dictionary."""
         res = []
         for record in self.data.values():
@@ -36,7 +36,7 @@ class AddressBook(UserDict):
         return res
 
     @classmethod
-    def from_dict(cls, data: List[Dict[str, ...]]) -> "AddressBook":
+    def from_dict(cls, data: List[Dict[str, str]]) -> "AddressBook":
         """Create an address book from a dictionary."""
         address_book = cls()
         for record in data:
@@ -78,7 +78,7 @@ class AddressBook(UserDict):
                 return record
         return None
 
-    def _get_users_birthday_in_current_year(self, data: Dict[str, ...]) -> List[Dict[str, Union[str, datetime]]]:
+    def _get_users_birthday_in_current_year(self, data: Dict[str, Any]) -> List[Dict[str, Union[str, datetime]]]:
         """Return the list of users with their birthday in the current year."""
         updated_users_list = []
         for user in data:
