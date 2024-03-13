445c83132eb68ad1ec5564d80d243e8762c5bd02
serhii <sbezuhlyi@tektelic.com>
2024-03-13 17:02:46 +0200

    add get-all birthdays logic

--- console_bot/command_handlers/default_command_handler.py
+++ console_bot/command_handlers/default_command_handler.py
@@ -122,10 +122,11 @@ class DefaultCommandHandler(BaseCommandHandler):
         
     def _delete(self, *args) -> str:
         """Delete an item from the address book or notebook."""
-        if args[0] == "contact":
-            return self._delete_contact()
-        elif args[0] == "note":
-            return self._delete_note()
+        command = args[0]
+        if command == "contact":
+            return self._delete_contact(*args[1:])
+        elif command == "note":
+            return self._delete_note(*args[1:])
         else:
             return "Invalid command, please try again."
 
@@ -140,10 +141,13 @@ class DefaultCommandHandler(BaseCommandHandler):
 
     def _get_all(self, *args) -> None:
         """Show all items in the address book or notebook."""
-        if args[0] == "contacts":
+        command = args[0]
+        if command == "contacts":
             self._get_contacts()
-        elif args[0] == "notes":
+        elif command == "notes":
             self._get_notes()
+        elif command == "birthdays":
+            self._get_birthdays_from_date(*args[1:])
         else:
             print("Invalid command, please try again.")
 
@@ -229,18 +233,20 @@ class DefaultCommandHandler(BaseCommandHandler):
             return f"Note with index {index} does not exist."
 
     @input_error_handler
-    def _delete_contact(self) -> str:
+    def _delete_contact(self, name: str = None) -> str:
         """Delete a contact from the address book."""
-        name = self.bot.prmt_session.prompt("Enter the name of the contact you want to delete: ")
+        if not name:
+            name = self.bot.prmt_session.prompt("Enter the name of the contact you want to delete: ")
         result: bool = self.bot.address_book.delete_record(name)
         if result:
             return f"Contact {name.capitalize()} has been deleted."
         return f"Contact {name.capitalize()} does not exist."
 
     @input_error_handler
-    def _delete_note(self) -> str:
+    def _delete_note(self, index: int = None) -> str:
         """Delete a note from the notebook."""
-        index = self.bot.prmt_session.prompt("Enter the index of the note you want to delete: ")
+        if not index:
+            index = self.bot.prmt_session.prompt("Enter the index of the note you want to delete: ")
         index = int(index)
         if self.bot.note_book.delete_note(index):
             return f"Note {index} has been deleted."
